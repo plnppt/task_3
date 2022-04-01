cfe6862e20272e452592d222d2498f978383bab5
plnppt <tockareva@list.ru>
2022-04-01 19:22:46 +0300

    #2

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+from scipy.linalg import solve_triangular
 import numpy as np
 
 
+def print_matrix(A, B):
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            print("{:3.0f}".format(A[i][j]), end=" ")
+        print('')
+
+    for i in range(len(B)):
+        print("{:15.0f}".format(B[i]))
+    print('')
+
+
+def method_cholesky(A, B):
+    for i in range(10000):
+        L = np.linalg.cholesky(A)
+        Y = solve_triangular(L, B, lower=True, check_finite=False)
+        X = solve_triangular(L, Y, lower=True, trans=1, check_finite=False)
+    print(X)
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    A = np.array([[81, -45, 45],
+                  [-45, 50, -15],
+                  [45, -15, 38]])
 
+    B = np.array([531, -460, 193])
+
+    print('\n' + 'The original SLAE:' + '\n')
+    print_matrix(A, B)
+    print('\n' + "The vector by Cholesky's method:" + '\n')
+    method_cholesky(A, B)
