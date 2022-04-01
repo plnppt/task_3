a23a463167c0c7932eab4e1488ee65ba2e79b6e9
plnppt <tockareva@list.ru>
2022-04-01 20:21:28 +0300

    #3

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ from scipy.linalg import solve_triangular
 import numpy as np
 
 
-def print_matrix(A, B):
+def print_matrix(A, b):
     for i in range(len(A)):
         for j in range(len(A[i])):
             print("{:3.0f}".format(A[i][j]), end=" ")
         print('')
 
-    for i in range(len(B)):
-        print("{:15.0f}".format(B[i]))
+    for i in range(len(b)):
+        print("{:15.0f}".format(b[i]))
     print('')
 
 
-def method_cholesky(A, B):
+def method_cholesky(A, b):
     for i in range(10000):
         L = np.linalg.cholesky(A)
-        Y = solve_triangular(L, B, lower=True, check_finite=False)
-        X = solve_triangular(L, Y, lower=True, trans=1, check_finite=False)
-    print(X)
+        y = solve_triangular(L, b, lower=True)
+        x = solve_triangular(L, y, lower=True, trans=1)
+    print(x)
 
 
 if __name__ == '__main__':
@@ -26,9 +26,9 @@ if __name__ == '__main__':
                   [-45, 50, -15],
                   [45, -15, 38]])
 
-    B = np.array([531, -460, 193])
+    b = np.array([531, -460, 193])
 
-    print('\n' + 'The original SLAE:' + '\n')
-    print_matrix(A, B)
-    print('\n' + "The vector by Cholesky's method:" + '\n')
-    method_cholesky(A, B)
+    print('\n' + 'the original SLAE:' + '\n')
+    print_matrix(A, b)
+    print('\n' + "the vector by Cholesky's method:" + '\n')
+    method_cholesky(A, b)
--- tests.py
+++ tests.py
@@ -1,9 +1,27 @@
 import unittest
+import numpy as np
+import main
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_method_1(self):
+        A = np.array([[6, 15, 55],
+                      [15, 55, 225],
+                      [55, 225, 979]])
+
+        b = np.array([76, 295, 1259])
+
+        self.assertEqual(main.method_cholesky(A, b), None)
+
+    def test_method_2(self):
+        A = np.array([[15, 3, 4, 5],
+                      [2, 16, 4, 5],
+                      [2, 3, 17, 5],
+                      [2, 3, 4, 18]])
+
+        b = np.array([13, -1, 17, -50])
+
+        self.assertEqual(main.method_cholesky(A, b), None)
 
 
 if __name__ == '__main__':
